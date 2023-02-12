ac6a67897bb2efa47a7e7df27f31b975b2e27562
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-02-12 13:57:24 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleOnClick = () => {
+    setIsClicked(true)
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">
+        {isClicked
+          ? "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          : ""}
+      </p>
+      <button id="click" onClick={handleOnClick}></button>
     </div>
   );
 }
-
 
 export default App;
